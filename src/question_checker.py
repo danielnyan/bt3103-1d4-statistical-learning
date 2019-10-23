1a168485d839a2a28bfd729c1adf059fec72c125
danielnyan <E0235108@u.nus.edu>
2019-10-23 18:40:53 +0800

    Part 2 of attempting to enable CORS

--- src/question_checker.py
+++ src/question_checker.py
@@ -27,7 +27,7 @@ def alset_api(postReq):
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "http://localhost:8080",
+            "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "Content-Type",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
         },
