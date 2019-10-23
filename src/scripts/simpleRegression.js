920f1bd110aee3c155495f3d8a7e7bdcc89f29b2
danielnyan <E0235108@u.nus.edu>
2019-10-23 19:25:03 +0800

    Figured out how to work with CORS. Will implement activities

--- src/scripts/simpleRegression.js
+++ src/scripts/simpleRegression.js
@@ -1,30 +1,19 @@
 console.log("Script loaded");
 
+var resultInfo = null;
+
 const getLambdaFeedback = async function() {
   let result = await new Promise(resolve => {
-    const nekoUrl = "https://1b1u6ce6m6.execute-api.us-east-1.amazonaws.com/Prod/";
+    const nekoUrl = "https://1b1u6ce6m6.execute-api.us-east-1.amazonaws.com/Prod/lambda_handler";
     const xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
       if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
         resolve(xmlHttp.responseText);
     }
     xmlHttp.open("POST", nekoUrl, true); 
-    xmlHttp.setRequestHeader("Content-type", "application/json");
-    xmlHttp.send({
-      editable: {
-        0: `test editable`
-      },
-      hidden: {
-        0: "test hidden \n test hidden 2"
-      },
-      shown: {
-        0: `test shown
-        test shown 2`
-      },
-      userToken: "asdf"
-    });
+    xmlHttp.send(JSON.stringify({userToken:"DtQmo", editable:{0:"asdf"}, hidden:{0:"asdf"}, shown:{0:"asdf"}}));
   });
-  return result;
+  resultInfo = JSON.parse(result);
 }
 
 let result = getLambdaFeedback();
