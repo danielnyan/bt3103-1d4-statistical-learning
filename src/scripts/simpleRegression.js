f39b5f27aa301c20c2eb8dea2ef0caef96f78da1
danielnyan <E0235108@u.nus.edu>
2019-10-23 20:02:02 +0800

    Implemented first question

--- src/scripts/simpleRegression.js
+++ src/scripts/simpleRegression.js
@@ -16,4 +16,44 @@ const getLambdaFeedback = async function() {
   resultInfo = JSON.parse(result);
 }
 
-let result = getLambdaFeedback();
+getLambdaFeedback();
+
+const submit1a = async function() {
+  let answer = $("#answer1a").val()
+  if (answer === "7.82") {
+    $("#incorrect1a").slideUp(500);
+    $("#correct1a").slideDown(500);
+  } else {
+    $("#incorrect1a").slideDown(500);
+    $("#correct1a").slideUp(500);
+  }
+  /*
+  let result = await new Promise(resolve => {
+    const nekoUrl = "https://1b1u6ce6m6.execute-api.us-east-1.amazonaws.com/Prod/lambda_handler";
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        resolve(xmlHttp.responseText);
+    }
+    xmlHttp.open("POST", nekoUrl, true); 
+    xmlHttp.send(JSON.stringify({userToken:"DtQmo", editable:{0:"asdf"}, hidden:{0:"asdf"}, shown:{0:"asdf"}}));
+  });
+  resultInfo = JSON.parse(result);*/
+}
+
+const submit1b = async function() {
+  let answer = $("#answer1b").text()
+  console.log(JSON.stringify(answer));
+  /*
+  let result = await new Promise(resolve => {
+    const nekoUrl = "https://1b1u6ce6m6.execute-api.us-east-1.amazonaws.com/Prod/lambda_handler";
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        resolve(xmlHttp.responseText);
+    }
+    xmlHttp.open("POST", nekoUrl, true); 
+    xmlHttp.send(JSON.stringify({userToken:"DtQmo", editable:{0:"asdf"}, hidden:{0:"asdf"}, shown:{0:"asdf"}}));
+  });
+  resultInfo = JSON.parse(result);*/
+}
