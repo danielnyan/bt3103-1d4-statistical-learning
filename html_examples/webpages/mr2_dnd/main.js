d71ceed05167d5d7ef85b31b3640a48fceee735b
Blanc <coldsonata@gmail.com>
2019-10-24 22:58:23 +0800

    Made the p-value reset to 0 if you removed everything

--- html_examples/webpages/mr2_dnd/main.js
+++ html_examples/webpages/mr2_dnd/main.js
@@ -6,7 +6,7 @@ Vue.component("variableblock", {
     </span>
   `,
     methods: {
-        initialize : async function(e) {
+        initialize: async function(e) {
             this.$emit("initialize", this);
         },
         handleMouseDown(e) {
@@ -76,14 +76,15 @@ Vue.component("variableblock", {
 });
 
 Vue.component("bar", {
-  props: ["name", "val"],
-  template: `
+    props: ["name", "val"],
+    template: `
     <p>{{name}} : p-value = {{val}}
-  `,/*
-  watch: {
-    val(newValue, oldValue) {
-    }
-  }*/
+  `,
+    /*
+      watch: {
+        val(newValue, oldValue) {
+        }
+      }*/
 });
 
 let variables = new Vue({
@@ -108,43 +109,46 @@ let variables = new Vue({
             { name: "origin" }
         ],
         selected: [],
-        properties: [{name:"Adjusted R-squared", val:0}],
+        properties: [{ name: "Adjusted R-squared", val: 0 }],
     },
     methods: {
         swapVar(name) {
             for (let index in this.unselected) {
-              if (this.unselected[index].name === name) {
-                let variable = this.unselected[index];
-                this.selected.push(variable);
-                this.unselected.splice(index, 1);
-                this.submitToLambda(variable.name, "add");
-                return;
-              }
+                if (this.unselected[index].name === name) {
+                    let variable = this.unselected[index];
+                    this.selected.push(variable);
+                    this.unselected.splice(index, 1);
+                    this.submitToLambda(variable.name, "add");
+                    return;
+                }
             }
             for (let index in this.selected) {
-              if (this.selected[index].name === name) {
-                let variable = this.selected[index];
-                this.unselected.push(variable);
-                this.selected.splice(index, 1);
-                this.submitToLambda(variable.name, "remove");
-                return;
-              }
+                if (this.selected[index].name === name) {
+                    let variable = this.selected[index];
+                    this.unselected.push(variable);
+                    this.selected.splice(index, 1);
+                    this.submitToLambda(variable.name, "remove");
+                    return;
+                }
             }
         },
         submitToLambda(variable, operation) {
-          if (operation === "remove") {
-            for (let index in this.properties) {
-              if (this.properties[index].name === variable) {
-                this.properties.splice(index, 1);
-                break;
-              }
+            if (operation === "remove") {
+                for (let index in this.properties) {
+                    if (this.properties[index].name === variable) {
+                        this.properties.splice(index, 1);
+                        break;
+                    }
+                }
+            } else if (operation === "add") {
+                this.properties.push({ name: variable, val: 0 });
             }
-          } else if (operation === "add") {
-            this.properties.push({name:variable,val:0});
-          }
-          for (let item of this.properties) {
-            item.val = Math.random();
-          }
+            for (let item of this.properties) {
+                item.val = Math.random();
+            }
+            if (this.properties.length == 1) {
+                this.properties[0].val = 0;
+            }
         },
         initialise(e) {
             console.log("Initialised");
