bafe0958646cbb863b4b862e6837db365f8c56eb
Blanc <coldsonata@gmail.com>
2019-10-25 15:08:51 +0800

    round2

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ rm -f lambda-deploy.zip
 tar -czvf lambda-deploy-overlay.tgz ./
 aws s3 cp --acl public-read lambda-deploy-overlay.tgz "$OVERLAY_S3URL"
 cd src/site-packages; python3 -m pip install --target=./ pandas; cd ../..
-cd src; ip -r ../lambda-deploy.zip *
+cd src; zip -r ../lambda-deploy.zip *
 cd ..
 
 aws cloudformation validate-template \
