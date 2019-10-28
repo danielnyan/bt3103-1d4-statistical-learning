496bca7798f856a7dad603f0751ed038d0df85f2
Blanc <coldsonata@gmail.com>
2019-10-29 02:41:16 +0800

    Update lambda_handler.py

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -1,17 +1,31 @@
 import json
+import os
+import boto3
+from datetime import datetime
 from content_parser import *
 from question_checker import *
-import statsmodels.formula as smf
-import numpy as np
-import pandas as pd
 
+dynamodb = boto3.resource('dynamodb')
+
+def save_logs(logData):
+    timestamp = str(datetime.utcnow().timestamp())
+
+    table = dynamodb.Table('lambdalogs')
+    
+    log = logData.copy() 
+    log['logId'] = str(timestamp) 
+    log['createdAt'] = timestamp
+
+    table.put_item(Item=log)
 
 def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
     method = event.get('httpMethod', {})
     if method == 'GET':
         path = event.get('path')
         return content_parser(path)
     if method == 'POST':
-        postReq = json.loads(event.get('body', {}))
+        postReq = json.loads(event.get('body', {})) 
+        save_logs(postReq)
         return question_checker(postReq)
         
