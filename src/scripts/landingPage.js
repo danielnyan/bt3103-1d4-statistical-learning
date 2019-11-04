1fbc5fbd8f3099c92c1f84e0e3d2083f59943c69
danielnyan <E0235108@u.nus.edu>
2019-11-04 11:22:58 +0800

    Messing with permissions: round 2

---
+++ src/scripts/landingPage.js
@@ -0,0 +1,8 @@
+/* To do:
+performance.navigation.type = 1 indicates a refresh
+In Python, uuid4() in the uuid library makes a key.
+Thus, we should also make a table that stores user ID 
+and the questions they completed.
+We can either use window.name or sessionStorage.getItem / 
+setItem to persist the user ID across webpages.
+*/
