ba4b3ec213253adc8145f987deb9495e18912dc3
Blanc <coldsonata@gmail.com>
2019-11-05 17:40:28 +0800

    idk what im doing lmao i hope it works

---
+++ src/scripts/multipleRegression.js
@@ -0,0 +1,52 @@
+let fuee = new Vue({
+    el: '#app',
+    data: {
+        q2a: {
+            correct: "Correct!",
+            incorrect: "Incorrect!",
+            status: "none",
+            solved: false
+        },
+    },
+    methods: {
+        submit2a: async function() {
+            let entered = parseCode("answer2a");
+            this.q1b.status = "checking";
+            let result = await new Promise((resolve, reject) => {
+                const nekoUrl = window.location.origin + "/Prod/lambda_handler"
+                const xmlHttp = new XMLHttpRequest();
+                xmlHttp.onreadystatechange = function() {
+                    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                        resolve(xmlHttp.responseText);
+                    }
+                }
+                xmlHttp.open("POST", nekoUrl, true);
+                xmlHttp.send(JSON.stringify({
+                    questionId: "2a",
+                    answer: JSON.stringify(entered)
+                }));
+            });
+            let resultInfo = JSON.parse(result);
+            if (resultInfo.correct) {
+                this.q2a.status = "correct";
+                this.q2a.solved = true;
+            } else {
+                this.q2a.status = "incorrect";
+            }
+        },
+        nextPage() {
+            if (this.q2a.solved || $("#prompt").css("display") !== "none") {
+                window.location.href = "multiple_regression2.html"
+            } else {
+                $("#prompt").slideDown(500);
+                setTimeout(this.hidePrompt, 5000)
+            }
+        },
+        hidePrompt() {
+            $("#prompt").slideUp(500);
+        }
+    },
+    mounted() {
+        initializeCodeblocks(["answer2a"]);
+    }
+})
