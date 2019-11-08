62e6bccaee2f50a92205e99c7c11157a35aaf3a7
Blanc <coldsonata@gmail.com>
2019-11-08 21:10:15 +0800

    updated categorical

--- src/question_checker.py
+++ src/question_checker.py
@@ -15,6 +15,8 @@ def question_checker(postReq):
         return question_2b(postReq)
     elif question_id == "5a":
         return question_5a(postReq)
+    elif question_id == '6a':
+        return question_6a(postReq)
 
 def alset_api(postReq):
     # Retrieve the contents from the request
@@ -145,6 +147,50 @@ def question_5a(postReq):
         if "Intercept" in gotten["output"]["predictors"]:
             gotten["output"]["predictors"].remove("Intercept")
         gotten["output"]["predictors"] = sorted(gotten["output"]["predictors"])
+    solved = (expected == gotten["output"])
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
+        "body":  json.dumps({
+            "correct": solved,
+            "expected": expected,
+            "gotten": gotten,
+            "answer": answer
+        })
+    }
+
+def question_6a(postReq):
+    answer = json.loads(postReq["answer"])
+    expected = {'cols':['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp',
+       'Parch', 'Ticket', 'Fare', 'Embarked', 'C85', 'C123', 'E46', 'G6', 'C103', 'D56', 'A6',
+       'C23 C25 C27', 'B78', 'D33', 'B30', 'C52', 'B28', 'C83', 'F33',
+       'F G73', 'E31', 'A5', 'D10 D12', 'D26', 'C110', 'B58 B60', 'E101',
+       'F E69', 'D47', 'B86', 'F2', 'C2', 'E33', 'B19', 'A7', 'C49', 'F4',
+       'A32', 'B4', 'B80', 'A31', 'D36', 'D15', 'C93', 'C78', 'D35',
+       'C87', 'B77', 'E67', 'B94', 'C125', 'C99', 'C118', 'D7', 'A19',
+       'B49', 'D', 'C22 C26', 'C106', 'C65', 'E36', 'C54',
+       'B57 B59 B63 B66', 'C7', 'E34', 'C32', 'B18', 'C124', 'C91', 'E40',
+       'T', 'C128', 'D37', 'B35', 'E50', 'C82', 'B96 B98', 'E10', 'E44',
+       'A34', 'C104', 'C111', 'C92', 'E38', 'D21', 'E12', 'E63', 'A14',
+       'B37', 'C30', 'D20', 'B79', 'E25', 'D46', 'B73', 'C95', 'B38',
+       'B39', 'B22', 'C86', 'C70', 'A16', 'C101', 'C68', 'A10', 'E68',
+       'B41', 'A20', 'D19', 'D50', 'D9', 'A23', 'B50', 'A26', 'D48',
+       'E58', 'C126', 'B71', 'B51 B53 B55', 'D49', 'B5', 'B20', 'F G63',
+       'C62 C64', 'E24', 'C90', 'C45', 'E8', 'B101', 'D45', 'C46', 'D30',
+       'E121', 'D11', 'E77', 'F38', 'B3', 'D6', 'B82 B84', 'D17', 'A36',
+       'B102', 'B69', 'E49', 'C47', 'D28', 'E17', 'A24', 'C50', 'B42',
+       'C148']}
+    gotten = get_output(answer, "import scipy\nimport statsmodels\n", "", '{"cols":df.columns.unique()}')
+    gotten = json.loads(gotten)
+    if "cols" in expected:
+        expected["cols"] = sorted(expected["cols"])
+    if "cols" in gotten["output"]:
+        gotten["output"]["cols"] = sorted(gotten["output"]["cols"])
     solved = (expected == gotten["output"])
     return {
         "statusCode": 200,
---
+++ src/scripts/categorical.js
@@ -0,0 +1,68 @@
+let fuee = new Vue({
+    el: '#app',
+    data: {
+        q6a: {
+            correct: "Correct!",
+            incorrect: "Incorrect!",
+            status: "none",
+            solved: false
+        },
+    },
+    methods: {
+        submit6a: async function() {
+            let entered = parseCode("answer6a");
+            this.q5a.status = "checking";
+
+            let errorMessage = null;
+            let result = await submitToLambda("6a", JSON.stringify(entered))
+                .catch((err) => {
+                    errorMessage = err || "Network request failed";
+                });
+
+            if (errorMessage !== null) {
+                this.q5a.status = "incorrect";
+                this.q5a.incorrect = "There appears to be a network error. Message: " + errorMessage;
+            }
+
+            let resultInfo = JSON.parse(result);
+            console.log(resultInfo);
+            if (resultInfo.correct) {
+                this.q5a.status = "correct";
+                this.q5a.solved = true;
+            } else {
+                this.q5a.status = "incorrect";
+                this.q5a.incorrect = "Incorrect!";
+            }
+            if (resultInfo.gotten.error) {
+                this.q5a.incorrect += " It appears that there is an error. The message is as follows: ";
+                this.q5a.incorrect += resultInfo.gotten.output;
+            } else if (!resultInfo.correct) {
+                if (resultInfo.gotten.output.model) {
+                    this.q5a.incorrect += " You are regressing " + resultInfo.gotten.output.response + " against these variables: " +
+                        resultInfo.gotten.output.predictors;
+                } else {
+                    this.q5a.incorrect += " Are you sure you are using the correct model?";
+                }
+            }
+        },
+        nextPage() {
+            if ((this.q6a.solved) || $("#prompt").css("display") !== "none") {
+                window.location.href = "contents.html"
+            } else {
+                $("#prompt").slideDown(500);
+                setTimeout(this.hidePrompt, 5000)
+            }
+        },
+        hidePrompt() {
+            $("#prompt").slideUp(500);
+        }
+    },
+    mounted() {
+        initializeCodeblocks(["answer6a"]);
+        retrieveProgress().then((result) => {
+            if (result.includes("6a")) {
+                this.q6a.solved = true;
+            }
+        });
+    }
+})
